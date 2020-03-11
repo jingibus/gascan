af7f556aaab834d0fe0c6d7ddbb2eef5b20700db
Bill Phillips <jings.bill@gmail.com>
2020-03-10 22:12:54 -0700

    Add a destination folder for interning functions.

--- src/gascan/intern.clj
+++ src/gascan/intern.clj
@@ -18,11 +18,11 @@
 (def resources-folder (join "/" [project-folder "resources"]))
 
 (defn interned-filepath
-  [filepath folder-depth]
+  [filepath reldest folder-depth]
   (let [fileobj (as-file filepath)
         trimmed-filepath (replace filepath #"/*$" "")
         components (split trimmed-filepath #"/")
-        intern-components (take-last (+ folder-depth 1) components)
+        intern-components (concat reldest (take-last (+ folder-depth 1) components))
         intern-rel-filepath (join "/" intern-components)
         ]
     intern-rel-filepath))
@@ -32,13 +32,13 @@
   (join "/" [resources-folder intern-rel-filepath]))
 
 (defn intern-file!
-  ([filepath folder-depth]
+  ([filepath reldest folder-depth]
    (let [fileobj (as-file filepath)]
      (if (.isFile fileobj)
        (intern-file! filepath folder-depth (slurp fileobj))
        (throw (new java.io.IOException "Cannot interna a non-file")))))
-  ([filepath folder-depth contents]
-   (let [intern-rel-filepath (interned-filepath filepath folder-depth)
+  ([filepath reldest folder-depth contents]
+   (let [intern-rel-filepath (interned-filepath filepath reldest older-depth)
          intern-abs-filepath (intern-abs-filepath intern-rel-filepath)
          intern-fileobj (as-file intern-abs-filepath)]
      (do 
