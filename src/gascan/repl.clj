01c88d9164c9ea2401afb909f1f9e809da9074ab
Bill Phillips <jings.bill@gmail.com>
2020-03-21 14:06:11 -0700

    Improve debug tools in repl and introduce line break transformation.

--- src/gascan/repl.clj
+++ src/gascan/repl.clj
@@ -8,7 +8,8 @@
             [clojure.reflect :refer [reflect]]
             [clojure.string :refer [join]]
             [gascan.multimarkdown :refer [parse-multimarkdown-flat flexmark-options make-options]]
-            [clojure.tools.trace :refer [trace-ns untrace-ns]])
+            [clojure.tools.trace :refer [trace-ns untrace-ns trace-forms]]
+            [clojure.zip :as z])
   (:use [gascan.debug])
   (:gen-class))
 
@@ -43,17 +44,111 @@
                )
         children))))
 
+(defn enstructure-flexmark
+  [nodeable]
+  (let [children (getNodeChildren nodeable)]
+    (if (or (nil? children) (empty? children))
+      nodeable
+      (vec (cons nodeable (map enstructure-flexmark children))))))
+
+(defn deep-map-vec
+  ([f all-items]
+   (cond (not (coll? all-items))
+         (f all-items)
+         (empty? all-items)
+         all-items
+         :else
+         (let [[item & items] all-items
+               mapped-item (if (vector? item)
+                             (deep-map-vec f item)
+                             (f item))]
+           (vec (cons mapped-item (deep-map-vec f items)))))))
+
+(defn stringify
+  [enstructured-flexmark]
+  (deep-map-vec str enstructured-flexmark))
+
 (defn decrapinate-flexmark
   [nodeable]
-  (let [children (getNodeChildren nodeable)
-        simple-name (some-> nodeable class (.getSimpleName))]
-    (if (or (nil? children) (empty? children))
-      (str nodeable)
-      (vec (cons simple-name (map decrapinate-flexmark children))))))
-
+  (letfn [(simple-name [x] (-> x (class) (.getSimpleName)))
+          (stringify 
+            [structured-node]
+            (if (vector? structured-node)
+              (let [[nodeable & children] structured-node]
+                (vec (cons (simple-name nodeable) 
+                           (map stringify children))))
+              (str structured-node)))]
+    (-> nodeable
+        enstructure-flexmark
+        stringify)))
 
 (defn test-parse-with-options
   [options test-case]
   (let [path (:path test-case)
         new-contents (parse-multimarkdown-flat options path)]
     (decrapinate-flexmark new-contents)))
+
+(defn test-parse-with-postprocessor
+  [postprocessor test-case]
+  (let [path (:path test-case)
+        new-contents (-> path (monitorv-> "path") parse-multimarkdown-flat (monitorv-> "parsed") postprocessor)]
+    (assoc test-case 
+           :path path 
+           :contents new-contents)))
+
+(defn split-line-breaks
+  "Given a Flexmark AST that has been translated into a tree of vectors, 
+  this splits all paragraphs with line breaks into separate paragraphs."
+  [enstructured-node]
+  (letfn [(is-line-break?
+            [node] 
+            (or (instance? com.vladsch.flexmark.ast.SoftLineBreak node) 
+                (instance? com.vladsch.flexmark.ast.HardLineBreak node)))
+          (remove-while
+            ;; Removes nodes while a condition on the node is true.
+            [loc while?]
+            (loop [loc loc]
+                (if (while? (z/node loc))
+                  (recur (z/remove loc))
+                  loc)))
+          (find-loc
+            [loc stop?]
+            (->> loc
+                 (iterate z/up)
+                 (filter stop?)
+                 first))
+          (show-node-and-right-siblings
+            ;; For debugging.
+            [node]
+            (str (z/node node) ": " (-> node z/rights stringify)))
+          (show-node-and-siblings
+            [node]
+            (str (-> node z/lefts stringify) " :"
+             ;(->> node z/lefts (map str) list) " :" 
+                 (stringify (z/node node)) 
+                 ": " (-> node z/rights stringify)))]
+    (loop [loc (z/vector-zip enstructured-node)]
+      (cond (z/end? loc)
+            ;; If we've finished walking the tree, yield the edited tree
+            (z/root loc)
+            (is-line-break? (z/node loc))
+            ;; If this node is a line break, then create a new paragraph node
+            ;; with all its rightmost siblings, remove those rightmost siblings, 
+            ;; pop up to the parent of the enclosing Paragraph, and add the new node
+            ;; after that.
+            (let [new-node (vec (cons (new com.vladsch.flexmark.ast.Paragraph)
+                                      (z/rights loc)))
+                  left-sibling (-> loc z/left z/node)
+                  edited (-> loc
+                             (monitorv-> "Editing:" show-node-and-siblings)
+                             z/rightmost
+                             (remove-while #(not (= % (z/node loc))))
+                             (monitorv-> "Removed right children of" show-node-and-right-siblings)
+                             z/remove
+                             (find-loc #(= left-sibling (z/node %)))
+                             z/up
+                             (monitorv-> "And now we insert a node to the right here:" show-node-and-siblings)
+                             (z/insert-right new-node))]
+              (recur (z/next edited)))
+            :else
+            (recur (z/next loc))))))
