a7a86b2281bccfa062d9057d41d3ae973cf69b42
Bill Phillips <jings.bill@gmail.com>
2020-03-23 19:19:55 -0700

    Add hiccup, sort deps

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
-                 [org.clojure/clojure "1.10.1"] 
                  [cli-matic "0.3.11"]
+                 [clojure.java-time "0.3.2"]
                  [com.vladsch.flexmark/flexmark-all "0.60.2"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/clojure "1.10.1"] 
                  [org.clojure/tools.trace "0.7.10"]
-                 [clojure.java-time "0.3.2"]]
+                 ]
   :main ^:skip-aot gascan.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
