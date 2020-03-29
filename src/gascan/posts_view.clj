5bc9d8d3e0612dacfe8126a89634804722e393bd
Bill Phillips <jings.bill@gmail.com>
2020-03-29 10:47:27 -0700

    Basic implementation and examples for route-post and find-post
    Does not yet migrate the test hacky code into tests.

--- src/gascan/posts_view.clj
+++ src/gascan/posts_view.clj
@@ -1,5 +1,10 @@
 (ns gascan.posts-view
   (:require [clojure.string :as string]
+            [gascan.ast :as ast]
+            [gascan.ast :as ast]
+            [gascan.intern :as intern]
+            [gascan.multimarkdown :as mm]
+            [gascan.multimarkdown :as mm]
             [gascan.posts :as posts])
   (:use [gascan.debug]))
 
@@ -24,15 +29,47 @@
             (let [matching-post (normalized-id (route-entitled post))]
               (= (select-keys matching-post (keys search-map))
                  (normalized-id search-map))))]
-    (println "finding:" search-map)
     (first  
      (->> (posts/posts)
           (filter matches)))))
 
-(defn route-post
-  [{:keys [id title] :as all}]
-  (apply find-post 
-         (monitor-> (list (into {} (filter #(second %) all))))))
+(defn render-markdown
+  [relpath]
+  (let [md-contents (some-> (intern/readable-file relpath) slurp)
+        massaged-mm (-> md-contents
+                        mm/parse-multimarkdown-str
+                        ast/build-scaffold-ast
+                        ast/split-line-breaks
+                        ast/restitch-scaffold-ast)]
+    (mm/render-multimarkdown massaged-mm)))
+
+  (defn route-post
+    [{:keys [id title] :as all}]
+    (let [non-null-args (into {} (filter #(second %) all))
+          {title             :title
+           timestamp         :timestamp
+           path              :markdown-rel-path} 
+          (find-post non-null-args)
+          ]
+      (render-markdown path)))
+(do
+  (use '[clojure.test])
+  (defn example
+    [desc f & args]
+    (println desc "args:" args "\n\tresult: " (apply f args)))
+  (comment
+
+    (def example-post (find-post {:title "blog-project"}))
+    (example "rendered", render-markdown (:markdown-rel-path example-post)))
+
+
+  
+ 
+  (example "non-null" (testing ((route-post {:title "blog-project"}))))
+  (example "null" route-post {:title "not-there"})
+  nil)
+
+(println (str (symbol route-post)))
 
 (comment
   (route-post {:title "blog-project"})
