f4678eab02eae51ebc8902102648b291d283be44
Bill Phillips <jings.bill@gmail.com>
2020-03-29 12:29:40 -0700

    Sketch up tests for posts_view.

--- src/gascan/posts_view.clj
+++ src/gascan/posts_view.clj
@@ -36,37 +36,38 @@
 (defn render-markdown
   [relpath]
   (let [md-contents (some-> (intern/readable-file relpath) slurp)
-        massaged-mm (-> md-contents
-                        mm/parse-multimarkdown-str
-                        ast/build-scaffold-ast
-                        ast/split-line-breaks
-                        ast/restitch-scaffold-ast)]
-    (mm/render-multimarkdown massaged-mm)))
+        massaged-mm (some-> md-contents
+                            mm/parse-multimarkdown-str
+                            ast/build-scaffold-ast
+                            ast/split-line-breaks
+                            ast/restitch-scaffold-ast)]
+    (println "massaged-mm" massaged-mm "md-contents" md-contents)
+    (when md-contents
+      (mm/render-multimarkdown massaged-mm))))
 
-  (defn route-post
-    [{:keys [id title] :as all}]
-    (let [non-null-args (into {} (filter #(second %) all))
-          {title             :title
-           timestamp         :timestamp
-           path              :markdown-rel-path} 
-          (find-post non-null-args)
-          ]
-      (render-markdown path)))
+(defn route-post
+  [{:keys [id title] :as all}]
+  (let [non-null-args (into {} (filter #(second %) all))
+        {title             :title
+         timestamp         :timestamp
+         path              :markdown-rel-path} 
+        (find-post non-null-args)
+        ]
+    (render-markdown path)))
 (do
   (use '[clojure.test])
+
   (defn example
     [desc f & args]
     (println desc "args:" args "\n\tresult: " (apply f args)))
-  (comment
 
-    (def example-post (find-post {:title "blog-project"}))
-    (example "rendered", render-markdown (:markdown-rel-path example-post)))
-
-
-  
- 
-  (example "non-null" (testing ((route-post {:title "blog-project"}))))
-  (example "null" route-post {:title "not-there"})
+  (testing "valid post yields some sort of HTML filled with <p>s"
+    (let [number-of-paras (some-> (route-post {:title "blog-project"}) 
+                                  (string/split #"<p>")
+                                  count)]
+      (is (> number-of-paras 5))))
+  (testing "invalid post yields nil"
+    (is (nil? (route-post {:title "not-there"}))))
   nil)
 
 (println (str (symbol route-post)))
