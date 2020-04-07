7276707ee29f6217270753ad09529ce34926f13e
Bill Phillips <jings.bill@gmail.com>
2020-04-06 22:56:13 -0700

    Translate image links in AST to inline HTML links

--- src/gascan/post_view.clj
+++ src/gascan/post_view.clj
@@ -7,7 +7,8 @@
             [gascan.multimarkdown :as mm]
             [gascan.posts :as posts]
             [gascan.session :as session]
-            [gascan.template :as tmpl])
+            [gascan.template :as tmpl]
+            [clojure.zip :as z])
   (:use [gascan.debug]))
 
 (defn to-kebab-case
@@ -35,16 +36,140 @@
      (->> (posts/posts)
           (filter matches)))))
 
+(defn link-entry
+  [loc]
+  (let [paragraph? (partial instance? com.vladsch.flexmark.ast.Paragraph)
+        linkref? (partial instance? com.vladsch.flexmark.ast.LinkRef)
+        text? (partial instance? com.vladsch.flexmark.ast.Text)
+        ]
+    (when (and (some-> loc z/down z/node paragraph?)
+               ;(do (println "found a para!") true)
+               (some-> loc z/down z/right z/down z/node linkref?)
+               ;(do (println "found a linkref!") true)
+               (some-> loc z/down z/right z/right z/node text?))
+      (let [reference (some-> loc 
+                              z/down z/right z/down z/node 
+                              .getReference
+                              .toString)
+            link-string (some-> loc z/down z/right z/right z/node .getChars .toString)
+            link (some-> link-string
+                         (clojure.string/replace #"^: " "")
+                         (clojure.string/replace #" width=.*" ""))]
+        {reference link}))))
+
+(defn extract-image-map
+  [[tags scaffold-ast]]
+  (loop [image-links {} 
+         loc (z/vector-zip scaffold-ast)]
+    (if (z/end? loc) [(assoc tags :image-map image-links) (z/root loc)]
+        (let [curr-entry (link-entry loc)]
+          (if curr-entry 
+            (recur (into image-links curr-entry) (z/remove loc))
+            (recur image-links (z/next loc)))))))
+
+(defn new-image-link
+  [referent]
+  (new com.vladsch.flexmark.ast.HtmlInline 
+       (com.vladsch.flexmark.util.sequence.CharSubSequence/of 
+        (str "<img src=\"" referent "\"/>"))))
+
+(defn apply-image-map
+  [[tags scaffold-ast]]
+  (let [image-map (:image-map tags)]
+    (loop [loc (z/vector-zip scaffold-ast)]
+      (cond (z/end? loc) [tags (z/root loc)]
+            (instance? com.vladsch.flexmark.ast.ImageRef (z/node loc))
+            (let [reference (some-> loc z/node .getReference .toString)
+                  referent (image-map reference)]
+              (if referent
+                (recur (-> loc 
+                           (z/replace (new-image-link referent))
+                           z/next))
+                (recur (z/next loc))))
+            :else
+            (recur (z/next loc))))))
+
+(comment
+  (->> (test-ast) 
+       ast/scaffold->tagged-scaffold
+       extract-image-map
+       apply-image-map
+       clojure.pprint/pprint)
+  (->> (test-ast) 
+       ast/scaffold->tagged-scaffold
+       extract-image-map
+       first
+       :image-map
+       clojure.pprint/pprint)
+  (->> (test-ast) 
+       ast/scaffold->tagged-scaffold
+       extract-image-map
+       first
+       :image-map
+       vals
+       first
+       ;show-methods
+       clojure.pprint/pprint)
+  (->> (test-ast)
+       transform-ast
+       ast/stringify
+       clojure.pprint/pprint)
+)
+(defn transform-ast
+  [ast]
+  (-> ast 
+      ast/split-line-breaks
+      ast/scaffold->tagged-scaffold
+      extract-image-map
+      apply-image-map
+      ast/tagged-scaffold->scaffold))
+
 (defn render-markdown
   [relpath]
   (let [md-contents (some-> (intern/readable-file relpath) slurp)
         massaged-mm (some-> md-contents
                             mm/parse-multimarkdown-str
                             ast/build-scaffold-ast
-                            ast/split-line-breaks
+                            transform-ast
                             ast/restitch-scaffold-ast)]
     (when md-contents
       (mm/render-multimarkdown massaged-mm))))
+
+(comment
+  (def image-test (first (filter #(= (:title %) "Image Test") (posts/fetch-posts))))
+  (defn test-ast 
+    []
+    (-> (first (filter #(= (:title %) "Image Test") (posts/fetch-posts)))
+        :markdown-rel-path
+        intern/readable-file
+        slurp
+        mm/parse-multimarkdown-str
+        ast/build-scaffold-ast))
+  (defn find-node
+    [pred ast]
+    (loop [loc (z/vector-zip ast)]
+      (cond (z/end? loc) nil
+            (pred (z/node loc)) (z/node loc)
+            :else (recur (z/next loc)))))
+
+  (->  (find-node (partial instance? com.vladsch.flexmark.ast.LinkRef) (test-ast))
+       .getReference)
+  (->>  (find-node (partial instance? com.vladsch.flexmark.ast.LinkRef) (test-ast))
+        .getClass
+        .getMethods
+        (sort-by #(.getName %))
+        (map str)
+        (map #(clojure.string/replace % #"com.vladsch.flexmark.(util.|)ast." ""))
+      clojure.pprint/pprint)
+
+  (->> (test-ast)
+    transform-ast
+    (ast/deep-map-vec #(some-> % type .getName))
+    clojure.pprint/pprint)
+  
+  
+
+  )
 
 (defn post->title-path
   [post]
@@ -57,6 +182,10 @@
 (defn post-by-id
   [id]
   (str "/posts/id/" id))
+
+(defn post-resources-view
+  [sess {:keys [id title] :as all} res-name]
+  )
 
 (defn post-view
   [sess {:keys [id title] :as all}]
@@ -90,7 +219,8 @@
     nil))
 
 (comment
-  (view-post {:title "blog-project"})
+  (post-view session/private-session {:title "blog-project"})
+  (clojure.pprint/pprint (post-view session/private-session {:title "image-test"}))
   (find-post {:title "blog-project"})
   (flatten (seq) {1 2})
   (posts/posts)
