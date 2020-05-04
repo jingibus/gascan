014f349493889a278f3442c6f33bc2f2ada3f169
Bill Phillips <jings.bill@gmail.com>
2020-05-03 21:33:27 -0700

    Strip options from multimarkdown and refer to flexmark module

--- src/gascan/multimarkdown.clj
+++ src/gascan/multimarkdown.clj
@@ -5,7 +5,8 @@
            [com.vladsch.flexmark.parser Parser ParserEmulationProfile]
            [com.vladsch.flexmark.util.data MutableDataSet])
   (:require [clojure.java.io :refer [as-file]]
-            [clojure.reflect :refer [reflect]])
+            [clojure.reflect :refer [reflect]]
+            [gascan.flexmark :as fm])
   (:use [gascan.debug])
   (:gen-class))
 
@@ -22,21 +23,9 @@
   (-> (make-options Parser/HEADING_NO_ATX_SPACE true)
       (.setFrom ParserEmulationProfile/MULTI_MARKDOWN)))
 
-(defn parse-multimarkdown-str
-  "Parses a string into a parsed flexmark object"
-  ([file-contents]
-   (parse-multimarkdown-str multimarkdown-options file-contents))
-  ([options file-contents]
-   (-> (Parser/builder options)
-         (.build)
-         (.parse file-contents))))
+(def parse-multimarkdown-str (partial fm/parse-str multimarkdown-options))
 
-(defn parse-multimarkdown-flat
-  "Parses a valid input to reader into a parsed flexmark object"
-  ([readable]
-   (parse-multimarkdown-str multimarkdown-options (slurp readable)))
-  ([options readable]
-   (parse-multimarkdown-str options(slurp readable))))
+(def parse-multimarkdown-flat (partial fm/parse-readable multimarkdown-options))
 
 (defn render-html
   ([options flexmark-document]
@@ -62,23 +51,19 @@
     (clojure.string/join "/" [dirpath x])))
 
 (defn parse-multimarkdown-directory
-  ([filepath]
-   (parse-multimarkdown-directory multimarkdown-options filepath))
-  ([options filepath]
-   (parse-multimarkdown-flat options (md-filepath-from-dir filepath))))
+  [filepath]
+  (parse-multimarkdown-flat (md-filepath-from-dir filepath)))
 
 (defn parse-multimarkdown
   "Yields a parsed flexmark Document instance."
-  ([filepath]
-   (parse-multimarkdown multimarkdown-options filepath))
-  ([options filepath]
-   (let [file-obj (as-file filepath)]
-     (cond (.isDirectory file-obj)
-           (parse-multimarkdown-directory options filepath)
-           (.isFile file-obj)
-           (parse-multimarkdown-flat options filepath)
-           :else
-           nil))))
+  [filepath]
+  (let [file-obj (as-file filepath)]
+    (cond (.isDirectory file-obj)
+          (parse-multimarkdown-directory filepath)
+          (.isFile file-obj)
+          (parse-multimarkdown-flat filepath)
+          :else
+          nil)))
 
 (defn flatten-iterator
   [iterator]
@@ -90,32 +75,4 @@
         (recur (conj values (.next iterator)))
         values))))
 
-(defn render-markdown
-  [document]
-  (let [renderer (-> (Formatter/builder multimarkdown-options) (.build))]
-    (.render renderer document)))
-
-(defn class-hierarchy
-  ([class-instance]
-   (class-hierarchy class-instance #{}))
-  ([class-instance traversed]
-   (if (traversed class-instance)
-     #{}
-     (let [traversed (conj traversed class-instance)
-           subsequent (apply clojure.set/union 
-                             (map #(class-hierarchy % traversed)
-                                  (bases class-instance)))]
-       (conj subsequent class-instance)))))
-
-(defn all-methods
-  [instance]
-  (let [all-classes (class-hierarchy (class instance))
-        total-reflection (apply merge-with 
-                                clojure.set/union 
-                                (map reflect all-classes))] 
-    (->> total-reflection
-         :members
-         (filter :return-type)
-         (map :name)
-         sort)))
-
+(def render-markdown (partial fm/render-markdown multimarkdown-options))
